e9a6ab93b659e2259f949e2153e2be459d6ec9c7
Giovanni Rufino <giovannirufino@gmail.com>
2022-04-10 08:25:58 -0400

    Set skeleton for CompanyList. Added api functionality to retrieve company list from API.

--- frontend/src/Components/Companies/CompanyList/CompanyList.js
+++ frontend/src/Components/Companies/CompanyList/CompanyList.js
@@ -4,9 +4,36 @@ import CompanyCard from '../CompanyCard/CompanyCard';
 import LoadingSpinner from '../../Common/LoadingSpinner/LoadingSpinner';
 
 function CompaniesList() {
+  const [companies, setCompanies] = useState(null);
+
+  // When page loads, requests a list of all companies from API
+
+  useEffect(function getCompaniesOnMount() {
+    find();
+  }, []);
+
+  // On find form submit, reload company list
+
+  async function find(name) {
+    let companies = await JoblyApi.getCompanies(name);
+    setCompanies(companies);
+  }
+
+  // Show spinner while waiting for API response
+
+  if (!companies) return <LoadingSpinner />;
+
   return (
     <div>
-      <LoadingSpinner></LoadingSpinner>
+      {companies.length ? (
+        <p>
+          {companies.map((company) => (
+            <li>{company.name}</li>
+          ))}
+        </p>
+      ) : (
+        <p>No results match find criteria!</p>
+      )}
     </div>
   );
 }
--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function Navigation() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <LinkContainer to="/">
+      <Container>
         <Navbar.Brand>Jobly</Navbar.Brand>
-      </LinkContainer>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/companies">
-            <Nav.Link>Companies</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/jobs">
-            <Nav.Link>Jobs</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        <Nav className="justify-content-end">
-          <LinkContainer to="/profile">
-            <Nav.Link>Profile</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/companies">
+              <Nav.Link>Companies</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/jobs">
+              <Nav.Link>Jobs</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav className="justify-content-end">
+            <LinkContainer to="/profile">
+              <Nav.Link>Profile</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
